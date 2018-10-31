45978b920969d49409f92eabaf27e9e2697071cc
Tom PoLáKoSz <polakosz@freemail.hu>
2018-10-31 21:13:31 +0100

    Removes unnecessary usings

--- src/NeptunPro.iOS/AppDelegate.cs
+++ src/NeptunPro.iOS/AppDelegate.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using Foundation;
+﻿using Foundation;
 using UIKit;
 
 namespace NeptunPro.iOS
 {
-    // The UIApplicationDelegate for the application. This class is responsible for launching the 
-    // User Interface of the application, as well as listening (and optionally responding) to 
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to
     // application events from iOS.
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
