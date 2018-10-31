e242db4c2b4ae254ebb0cca4021d1756223c106e
Tom PoLáKoSz <polakosz@freemail.hu>
2018-10-31 21:01:58 +0100

    Adds Regression test project

---
+++ tests/Regression/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NeptunPro.Tests.Regression
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
